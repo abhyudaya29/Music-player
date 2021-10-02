cfa2bd3ab41d5eacb8b4b63dfd7384965927f7d4
abhyudaya29 <90976445+abhyudaya29@users.noreply.github.com>
2021-10-02 09:17:13 +0530

    Add files via upload

---
+++ musicplyr.py
@@ -0,0 +1,48 @@
+import pygame
+import tkinter as tkr
+from tkinter.filedialog import askdirectory
+import os
+
+music_player = tkr.Tk()
+music_player.title("Buddhu Player")
+music_player.geometry("650x650")
+directory = askdirectory()
+os.chdir(directory)
+song_list = os.listdir()
+
+play_list = tkr.Listbox(music_player, font="Birthstone 12 bold", bg='Pink', selectmode=tkr.SINGLE)
+for item in song_list:
+    pos = 0
+    play_list.insert(pos, item)
+    pos += 1
+pygame.init()
+pygame.mixer.init()
+
+def play():
+    pygame.mixer.music.load(play_list.get(tkr.ACTIVE))
+    var.set(play_list.get(tkr.ACTIVE))
+    pygame.mixer.music.play()
+def stop():
+    pygame.mixer.music.stop()
+def pause():
+    pygame.mixer.music.pause()
+def unpause():
+    pygame.mixer.music.unpause()
+def volume():
+    pygame.mixer.music.volume()
+Button1 = tkr.Button(music_player, width=5, height=3, font="Birthstone 12 bold", text="PLAY", command=play, bg="Black", fg="white")
+Button2 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="STOP", command=stop, bg="Black", fg="white")
+Button3 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PAUSE", command=pause, bg="Black", fg="white")
+Button4 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="UNPAUSE", command=unpause, bg="Black", fg="white")
+Button5 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="Volume", command= volume, bg="Black", fg="white")
+var = tkr.StringVar() 
+song_title = tkr.Label(music_player, font="Helvetica 12 bold", textvariable=var)
+
+song_title.pack()
+Button1.pack(fill="x")
+Button2.pack(fill="x")
+Button3.pack(fill="x")
+Button4.pack(fill="x")
+#Button5.pack(fill="x")
+play_list.pack(fill="both", expand="yes")
+music_player.mainloop()
